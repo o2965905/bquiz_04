0e37907e1cf5f2f59f1f4f50c9d34d560bdef23b
o2965905 <o2965905@gmail.com>
2022-08-22 09:34:37 +0800

    題組四分類資料夾順便打好base檔案

---
+++ base.php
@@ -0,0 +1,105 @@
+<?php
+
+session_start();
+date_default_timezone_set('Asia/Taipei');
+
+
+class DB{
+    protected $table;
+    protected $dsn='mysql:host=localhost;charset=utf8;dbname=db04';
+    protected $pdo;
+
+    function __construct($table)
+    {
+        $this->table=$table;
+        $this->pdo=new PDO($this->dsn,'root','');
+    }
+
+    function all(...$arg){
+        $sql="select * from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                foreach($arg[0] as $key => $val){
+                    $tmp[]="`$key`='$val'";
+                }
+                $sql .= " where " .join(" && ",$tmp);
+            }else{
+                $sql .= $arg[0];
+            }
+        }
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+    function find($arg){
+        $sql="select * from $this->table where";
+        if(is_array($arg)){
+            foreach($arg as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+            $sql .= join(" && ",$tmp);
+        }else{
+            $sql .= " `id`='$arg'";
+        }
+        return $this->pdo->query($sql)->fetch(PDO::FETCH_ASSOC);
+    }
+    function save($array){
+        if(isset($array['id'])){
+            foreach($array as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+            $sql="update $this->table set ".join(',',$tmp)." where `id`='{$array['id']}'";
+        }else{
+            $sql="insert into $this->table (`".join("`,`",array_keys($array))."`) 
+                                        values('".join("','",$array)."')";
+        }
+        return $this->pdq->exec($sql);
+    }
+    function del($arg){
+        $sql="delete from $this->table where";
+        if(is_array($arg)){
+            foreach($arg as $key => $val){
+                $tmp[]="`$key`='$val'";
+            }
+            $sql .= join(" && ",$tmp);
+        }else{
+            $sql .= " `id`='$arg'";
+        }
+        return $this->pdo->exec($sql);
+    }
+    function math($math,$col,...$arg){
+        $sql="select $math($col) from $this->table ";
+        if(isset($arg[0])){
+            if(is_array($arg[0])){
+                foreach($arg[0] as $key => $val){
+                    $tmp[]="`$key`='$val'";
+                }
+                $sql .= " where " .join(" && ",$tmp);
+            }else{
+                $sql .= $arg[0];
+            }
+        }
+        if(isset($arg[1])){
+            $sql .= $arg[1];
+        }
+        return $this->pdo->query($sql)->fetchColumn();
+    }
+    function q($sql){
+        return $this->pdo->query($sql)->fetchAll(PDO::FETCH_ASSOC);
+    }
+}
+
+function dd($array){
+    echo "<pre>";
+    echo print_r($array);
+    echo "</pre>";
+}
+function to($url){
+    header('location:'.$url);
+}
+
+
+
+
+?>
