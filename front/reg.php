f1f4b29f649a28907de89c6b6e0a2ea8a9aac884
o2965905 <o2965905@gmail.com>
2022-08-22 13:30:30 +0800

    建構前台會員註冊畫面及重置(按鈕)的功能

---
+++ front/reg.php
@@ -0,0 +1,35 @@
+<!-- 前台-會員註冊 -->
+<h2 class="ct">會員註冊</h2>
+<table class="all" id="regform">
+    <tr>
+        <td class="tt ct">姓名</td>
+        <td class="pp"><input type="text" name="name" id="name"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp">
+            <input type="text" name="acc" id="acc">
+            <button>檢測帳號</button>
+        </td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電話</td>
+        <td class="pp"><input type="text" name="tel" id="tel"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">地址</td>
+        <td class="pp"><input type="text" name="addr" id="addr"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">電子信箱</td>
+        <td class="pp"><input type="text" name="email" id="email"></td>
+    </tr>
+</table>
+<div class="ct">
+    <button>註冊</button>
+    <button onclick="$('#regform input').val('')">重置</button>
+</div>
