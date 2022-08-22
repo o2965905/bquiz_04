8e6ca58c3fca0f55f4a2ec4aac848c9df9bd26d6
o2965905 <o2965905@gmail.com>
2022-08-22 11:05:57 +0800

    建構前台-會員登入與管理者登入畫面

--- front/admin.php
+++ front/admin.php
@@ -1,1 +1,16 @@
 <!-- 前台-管理者登入 -->
+<table class="all">
+    <tr>
+        <td class="tt ct">帳號</td>
+        <td class="pp"><input type="text" name="acc" id="acc"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">密碼</td>
+        <td class="pp"><input type="password" name="pw" id="pw"></td>
+    </tr>
+    <tr>
+        <td class="tt ct">驗證碼</td>
+        <td class="pp"><input type="text" name="code" id="code"></td>
+    </tr>
+</table>
+<div class="ct"><button>確認</button></div>
