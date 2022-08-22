4b790eab7299f76e265726eca97b31e006ba94a8
o2965905 <o2965905@gmail.com>
2022-08-22 11:33:40 +0800

    建構後台-頁尾版權畫面

--- back/bot.php
+++ back/bot.php
@@ -1,1 +1,16 @@
 <!-- 後台-頁尾版權管理 -->
+<h2 class="ct">編輯頁尾版權區</h2>
+<form action="?" method="post">
+    <table class="all">
+        <tr>
+            <td class="tt ct">頁尾先告內容</td>
+            <td class="pp">
+                <input type="text" name="bot" id="">
+            </td>
+        </tr>
+    </table>
+    <div class="ct">
+        <input type="submit" value="編輯">
+        <input type="reset" value="重置">
+    </div>
+</form>
