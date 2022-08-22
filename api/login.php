3c8e5cecb5a1caa4642fac1832c95573b1f75a9d
o2965905 <o2965905@gmail.com>
2022-08-22 15:30:45 +0800

    完成會員登入功能

---
+++ api/login.php
@@ -0,0 +1,13 @@
+<!-- 檢查會員登入 -->
+<?php
+include_once "../base.php";
+
+$DB=new DB($_GET['table']);
+$chk=$DB->math('count','id',['acc'=>$_GET['acc'],'pw'=>$_GET['pw']]);
+
+if($chk>0){
+    $_SESSION[$_GET['table']]=$_GET['acc'];
+    echo 1;
+}else{
+    echo 0;
+} 
--- front/login.php
+++ front/login.php
@@ -1,8 +1,6 @@
 <!-- 前台-會員登入 -->
 <h2>第一次購物</h2>
-<a href="?do=reg">
-    <img src="/icon/0413.jpg" alt="">
-</a>
+<a href="?do=reg"><img src="/icon/0413.jpg" alt=""></a>
 <h2>會員登入</h2>
 <table class="all">
     <tr>
@@ -16,20 +14,18 @@
     <tr>
         <td class="tt ct">驗證碼</td>
         <td class="pp">
-            <?php
+        <?php
             $a=rand(10,99);
             $b=rand(10,99);
-            echo $a . "+" . $b . "=";
+            echo $a . " + " . $b . " = ";
             $_SESSION['ans']=$a+$b;
-            ?>
-            <input type="text" name="code" id="code">
-        </td>
+        ?>
+        <input type="text" name="code" id="code"></td>
     </tr>
 </table>
 <div class="ct"><button onclick="login('mem')">確認</button></div>
 
 <script>
-
     function login(table){
         let ans=$("#code").val();
         let user={acc:$("#acc").val(),pw:$("#pw").val(),table}
@@ -37,12 +33,25 @@
         $.get("./api/ans.php",{ans},(chk)=>{
             console.log(chk)
             if(parseInt(chk)===1){
-                alert('正確');
+                 $.get("./api/login.php",user,(chk)=>{
+                    // console.log(chk)
+                     if(parseInt(chk)===1){
+                         switch(table){
+                             case 'mem':
+                                 location.href='index.php'
+                             break;
+                             case 'admin':
+                                 location.href='back.php'
+                             break;
+                         }
+                     }else{
+                        alert("帳號或密碼有誤")
+                     }
+                 })
             }else{
-                alert('驗證碼錯誤,請重新輸入');
+                alert("對不起，您輸入的驗證碼有誤請您重新登入")
             }
-
         })
-    }
 
+    }
 </script>
