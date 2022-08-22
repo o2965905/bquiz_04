f3f2e2ad8c8700ddf3eb8f48f009c810b1831387
o2965905 <o2965905@gmail.com>
2022-08-22 11:17:00 +0800

    建構後台-會員管理與管理員設置的畫面

--- back/admin.php
+++ back/admin.php
@@ -1,1 +1,20 @@
-後台頁面-管理者區
+<!-- 後台頁面-管理者區 -->
+<div class="ct">
+    <button>新增管理員</button>
+</div>
+
+<table class="all">
+    <tr class="tt ct">
+        <td>帳號</td>
+        <td>密碼</td>
+        <td>管理</td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
+<div class="ct">
+    <button onclick="location.href='index.php'">返回</button>
+</div>
--- back/mem.php
+++ back/mem.php
@@ -1,1 +1,19 @@
 <!-- 後台-會員管理 -->
+<h2 class="ct">會員管理</h2>
+<table class="all">
+    <tr class="tt ct">
+        <td>姓名</td>
+        <td>會員帳號</td>
+        <td>註冊日期</td>
+        <td>操作</td>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td>
+            <button>修改</button>
+            <button>刪除</button>
+        </td>
+    </tr>
+</table>
