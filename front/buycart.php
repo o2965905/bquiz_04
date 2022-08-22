793cb6cbe30b43d1bbf06d3cbda7303136758e35
o2965905 <o2965905@gmail.com>
2022-08-22 10:46:23 +0800

    建立前台-購物車畫面

--- front/buycart.php
+++ front/buycart.php
@@ -1,1 +1,22 @@
 <!-- 前台-購物車 -->
+<h2 class="ct">OOO的購物車</h2>
+<table>
+    <tr class="tt ct">
+        <th>編號</th>
+        <th>商品名稱</th>
+        <th>數量</th>
+        <th>庫存量</th>
+        <th>單價</th>
+        <th>小計</th>
+        <th>刪除</th>
+    </tr>
+    <tr class="pp ct">
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+        <td></td>
+    </tr>
+</table>
