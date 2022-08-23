d4afb27c81376e367edc2ff3cc9bff9cf1b200d7
o2965905 <o2965905@gmail.com>
2022-08-23 13:57:13 +0800

    後台選單加上權限管理的判斷

--- back.php
+++ back.php
@@ -26,11 +26,13 @@ include_once "base.php";
 		<div id="left" class="ct">
 			<div style="min-height:400px;">
 				<a href="?do=admin">管理權限設置</a>
-				<a href="?do=th">商品分類與管理</a>
-				<a href="?do=order">訂單管理</a>
-				<a href="?do=mem">會員管理</a>
-				<a href="?do=bot">頁尾版權管理</a>
-				<a href="?do=news">最新消息管理</a>
+                <?php
+                echo (in_array(1,$_SESSION['pr']))?"<a href='?do=th'>商品分類與管理</a>":"";
+                echo (in_array(2,$_SESSION['pr']))?"<a href='?do=order'>訂單管理</a>":"";
+                echo (in_array(3,$_SESSION['pr']))?"<a href='?do=mem'> 會員管理</a>":"";
+                echo (in_array(4,$_SESSION['pr']))?"<a href='?do=bot'>頁尾版權管理</a>":"";
+                echo (in_array(5,$_SESSION['pr']))?"<a href='?do=news'>最新消息管理</a>":"";
+                ?>
 				<a href="#" onclick="logout('admin')" style="color:#f00;">登出</a>
 			</div>
 		</div>
