43d2bc2bc171351851f93a0777a547bb77eef3a3
o2965905 <o2965905@gmail.com>
2022-08-26 13:27:05 +0800

    讀出相關資料顯示在結帳畫面上

--- front/checkout.php
+++ front/checkout.php
@@ -1,24 +1,27 @@
 <h2 class="ct">填寫資料</h2>
+<?php
+$mem=$Mem->find(['acc'=>$_SESSION['mem']]);
+?>
 <table class="all">
     <tr>
         <td class="tt ct">登入帳號</td>
-        <td class="pp"></td>
+        <td class="pp"><?=$mem['acc'];?></td>
     </tr>
     <tr>
         <td class="tt ct">姓名</td>
-        <td class="pp"><input type="text" name="name" id="name"></td>
+        <td class="pp"><input type="text" name="name" id="name" value="<?=$mem['name'];?>"></td>
     </tr>
     <tr>
         <td class="tt ct">電子信箱</td>
-        <td class="pp"><input type="text" name="email" id="email"></td>
+        <td class="pp"><input type="text" name="email" id="email" value="<?=$mem['email'];?>"></td>
     </tr>
     <tr>
         <td class="tt ct">聯絡地址</td>
-        <td class="pp"><input type="text" name="addr" id="addr"></td>
+        <td class="pp"><input type="text" name="addr" id="addr" value="<?=$mem['addr'];?>"></td>
     </tr>
     <tr>
         <td class="tt ct">聯絡電話</td>
-        <td class="pp"><input type="text" name="tel" id="tel"></td>
+        <td class="pp"><input type="text" name="tel" id="tel" value="<?=$mem['tel'];?>"></td>
     </tr>
 </table>
 <table class="all">
@@ -29,15 +32,24 @@
         <td>單價</td>
         <td>小計</td>
     </tr>
+    <?php
+    $sum=0;
+    foreach($_SESSION['cart'] as $id => $qt){
+        $row=$Goods->find($id)
+    ?>
     <tr class="pp tc">
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
-        <td></td>
+        <td><?=$row['name'];?></td>
+        <td><?=$row['no'];?></td>
+        <td><?=$qt;?></td>
+        <td><?=$row['price'];?></td>
+        <td><?=$row['price'] * $qt;?></td>
     </tr>
+    <?php
+    $sum=$row['price'] * $qt;
+    }
+    ?>
 </table>
-<div class="all ct tt">總價:</div>
+<div class="all ct tt">總價:<?=$sum;?></div>
 <div class="ct">
     <button onclick="checout()">確定送出</button>
     <button onclick="location.href='?do=buycart'">返回修改訂單</button>
