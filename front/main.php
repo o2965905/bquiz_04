56024a71e7b45c095bb5a812be3769d54e780b21
o2965905 <o2965905@gmail.com>
2022-08-26 09:46:03 +0800

    在右側商品區判斷要顯示的商品類別

--- front/main.php
+++ front/main.php
@@ -1,5 +1,19 @@
 <!-- 前台頁面-商品區 -->
-<h2>全部商品 > 流行皮件</h2>
+<?php
+if($_GET['type']!=0){
+    $type=$Type->find($_GET['type']);
+    if($type['parent']==0){
+        $typebig=$type['name'];
+    }else{
+        $typemid=" > ".$type['name'];
+        $typebig=$Type->find($type['parent'])['name'];
+    }
+}else{
+$typebig="全部商品";
+}
+?>
+
+<h2><span><?=$typebig;?></span><span><?=$typemid??'';?></span></h2> 
 <div style="width:90%;margin:auto;display:flex;" class="pp">
     <div style="padding:1rem;width:35%;text-align:center">
         <img style="width:80%;" src="./icon/0403.jpg" alt="">
